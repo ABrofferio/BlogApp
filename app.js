e54a35e47e837be5a35a78af6a83f629f3118e82
Aja Brofferio <aja.brofferio@gmail.com>
2018-09-15 16:03:06 -0700

    initializing Blog App

---
+++ app.js
@@ -0,0 +1,107 @@
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+mongoose.connect("mongodb://localhost/blog_app", { useNewUrlParser: true });
+
+//APP CONFIG
+//for creating the Public Views folder structure
+app.use(express.static("public"));
+
+//for using bodyParser
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+//MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+	title:String,
+	author: String,
+	imageUrl: String,
+	post: String
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTFUL ROUTES
+
+//Home page
+app.get("/", function(req, res){
+res.redirect("/blogs");
+});
+
+//INDEX
+app.get("/blogs", function(req,res){
+Blog.find({},function(err, blogItems){
+	if(err){
+	console.log(err)
+	}else{
+//	console.log(blogItems);
+	res.render("index", {blogs:blogItems});
+	}
+})
+})
+
+//NEW
+app.get("/blogs/new", function(req, res){
+res.render("new");
+})
+
+//CREATE
+app.post("/blogs", function(req, res){
+Blog.create({
+	title:req.body.blogTitle,
+        author: req.body.blogAuthor,
+        imageUrl: req.body.blogImage,
+        post: req.body.blogPost
+	},function(err,blogItem){
+		if(err){
+			console.log(err);
+		}else{
+//			console.log("created new blog object");
+//			console.log(blogItem);
+			res.redirect("/blogs");
+		}
+})
+})
+
+//SHOW
+app.get("/blogs/:id", function(req, res){
+Blog.findById(req.params.id, function(err, blogItem){
+	if(err){
+	console.log(err)
+	}else{
+	res.render("show", {blog:blogItem});
+	}
+})
+})
+
+//EDIT
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, blogItem){
+		if(err){
+		console.log(err)
+		}else{
+		res.render("edit",{blog:blogItem})
+		}
+})
+})
+
+//UPDATE
+app.post("/blogs/:id", function(req, res){
+Blog.findOneAndUpdate({_id:req.params.id},{ $set:{
+	title:req.body.revisedTitle,
+        author: req.body.revisedAuthor,
+        imageUrl: req.body.revisedImage,
+        post: req.body.revisedPost
+}}, {new:true}, function(err, blogItem){
+	if(err){
+	console.log(err);
+	}else{
+	console.log(blogItem);
+	res.redirect("/blogs/"+req.params.id);
+	}
+})
+})
+app.listen(3000, function(){
+console.log("The Server is up and running listening on port 3000")});
