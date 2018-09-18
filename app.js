24d8ee4ebea9022a96f3fb206284bfd6da54ba61
Aja Brofferio <aja.brofferio@gmail.com>
2018-09-18 11:48:39 -0700

    fleshing out the destroy method, sanitizing inputs, styling tweaks

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
 
 mongoose.connect("mongodb://localhost/blog_app", { useNewUrlParser: true });
 
 //APP CONFIG
-//for creating the Public Views folder structure
 app.use(express.static("public"));
-
-//for using bodyParser
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.set("view engine", "ejs");
-
+app.use(methodOverride("_method"));
 //MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
 	title:String,
@@ -57,6 +57,8 @@ Blog.create({
 	},function(err,blogItem){
 */
 /* above can be replaced by creating each of the input values as properties of an object in name attribute of input field in views*/
+/*sanitizing post inputs */
+req.body.blog.post = req.sanitize(req.body.blog.post);
 Blog.create(req.body.blog, function(err, blogItem){
 		if(err){
 			console.log(err);
@@ -89,13 +91,18 @@ app.get("/blogs/:id/edit", function(req, res){
 })
 
 //UPDATE
-app.post("/blogs/:id", function(req, res){
+app.put("/blogs/:id", function(req, res){
+/*
 Blog.findOneAndUpdate({_id:req.params.id},{ $set:{
 	title:req.body.revisedTitle,
         author: req.body.revisedAuthor,
         imageUrl: req.body.revisedImage,
         post: req.body.revisedPost
 }}, {new:true}, function(err, blogItem){
+*/
+/*sanitizing inputs*/
+req.body.revisedBlog.post = req.sanitize(req.body.revisedBlog.post);
+Blog.findOneAndUpdate({_id:req.params.id},{ $set:req.body.revisedBlog}, {new:true}, function(err, blogItem){
 	if(err){
 	console.log(err);
 	}else{
@@ -104,5 +111,17 @@ Blog.findOneAndUpdate({_id:req.params.id},{ $set:{
 	}
 })
 })
+
+//DELETE
+app.delete("/blogs/:id", function(req,res){
+Blog.findByIdAndRemove(req.params.id, function(err){
+	if(err){
+	console.log(err);
+	}else{
+	res.redirect("/blogs");
+	}
+})
+})
+
 app.listen(3000, function(){
 console.log("The Server is up and running listening on port 3000")});
